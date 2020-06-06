1a1b631764d71ce0674784a5c5a6cda591434cb2
amber-tamara <ambertjansen@gmail.com>
2020-06-06 18:39:38 +0100

    Created input form and button to search for GIF's

---
+++ script.js
@@ -0,0 +1,41 @@
+// lceU9yKlVaHVuU0UMn3398cd9EhBj6Jh;
+
+// var xhr = fetch(
+//   "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=lceU9yKlVaHVuU0UMn3398cd9EhBj6Jh&limit=5"
+// );
+// xhr.done(function (data) {
+//   console.log("success got data", data);
+// });
+
+const gifForm = document.querySelector("#gif-form");
+gifForm.addEventListener("submit", fetchGiphs);
+const result = document.querySelector(".grid");
+
+function fetchGiphs(e) {
+  e.preventDefault();
+  const searchTerm = document.querySelector(".search").value;
+  fetch(
+    `http://api.giphy.com/v1/gifs/search?&q=${searchTerm}&limit=10&api_key=lceU9yKlVaHVuU0UMn3398cd9EhBj6Jh`
+  )
+    .then((response) => response.json())
+    .then((res) => res.data)
+    .then((data) => {
+      let dataArray = data;
+
+      showGiphs(dataArray);
+    });
+
+  function showGiphs(dataArray) {
+    let HTMLPayload = "";
+
+    for (let i = 0; i < dataArray.length; i++) {
+      const img = `
+        <div class="grid-item">
+          <img src="${dataArray[i].images.original.url}" />
+        </div>`;
+
+      HTMLPayload += img;
+    }
+    result.innerHTML = HTMLPayload;
+  }
+}

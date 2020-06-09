4779e8fe1722b2aefd3afc39dd9c637705b17756
amber-tamara <ambertjansen@gmail.com>
2020-06-09 23:15:23 +0100

    no results div added

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-// lceU9yKlVaHVuU0UMn3398cd9EhBj6Jh;
-
-// var xhr = fetch(
-//   "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=lceU9yKlVaHVuU0UMn3398cd9EhBj6Jh&limit=5"
-// );
-// xhr.done(function (data) {
-//   console.log("success got data", data);
-// });
-
+let loading = false;
+let timeout = null;
+const hide = document.querySelector(".hide");
 const gifForm = document.querySelector("#gif-form");
-gifForm.addEventListener("submit", fetchGiphs);
+const banana = document.querySelector(".banana");
+const intro = document.querySelector(".intro");
 const result = document.querySelector(".grid");
 
+gifForm.addEventListener("keyup", function (e) {
+  clearTimeout(timeout);
+  timeout = setTimeout(function () {
+    fetchGiphs();
+  }, 600);
+});
+
 function fetchGiphs(e) {
-  e.preventDefault();
   const searchTerm = document.querySelector(".search").value;
+  console.log(searchTerm);
+  if (searchTerm.length < 1) {
+    console.log("ending function");
+    result.classList.add("ramama");
+    banana.classList.add("ramama");
+    intro.classList.remove("ramama");
+    return;
+  }
+  console.log("hey I should not be running, and I like pie");
   fetch(
     `http://api.giphy.com/v1/gifs/search?&q=${searchTerm}&limit=10&api_key=lceU9yKlVaHVuU0UMn3398cd9EhBj6Jh`
   )
@@ -21,8 +31,20 @@ function fetchGiphs(e) {
     .then((res) => res.data)
     .then((data) => {
       let dataArray = data;
-
-      showGiphs(dataArray);
+      console.log(dataArray.length);
+      if (dataArray.length === 0 && loading === false) {
+        console.log("yay");
+        banana.classList.remove("ramama");
+        result.classList.add("ramama");
+        intro.classList.add("ramama");
+      } else {
+        console.log("poo");
+        console.log(loading);
+        result.classList.remove("ramama");
+        banana.classList.add("ramama");
+        intro.classList.add("ramama");
+        showGiphs(dataArray);
+      }
     });
 
   function showGiphs(dataArray) {
@@ -33,9 +55,9 @@ function fetchGiphs(e) {
         <div class="grid-item">
           <img src="${dataArray[i].images.original.url}" />
         </div>`;
-
       HTMLPayload += img;
     }
+
     result.innerHTML = HTMLPayload;
   }
 }

3c5102277145bbded4b8d0e4305cb0d39316a036
amber-tamara <ambertjansen@gmail.com>
2020-06-18 22:48:31 +0100

    created modal and loading animation

--- script.js
+++ script.js
@@ -5,59 +5,135 @@ const gifForm = document.querySelector("#gif-form");
 const banana = document.querySelector(".banana");
 const intro = document.querySelector(".intro");
 const result = document.querySelector(".grid");
+const apiUrl = "http://api.giphy.com/v1/gifs/";
+const data = [];
+let paginate = 15;
+const limit = 15;
+let searchTerm = document.querySelector(".search").value;
+const box = document.querySelector(".box");
+const loding = document.querySelectorAll(".circle");
+console.log(loding);
 
 gifForm.addEventListener("keyup", function (e) {
   clearTimeout(timeout);
   timeout = setTimeout(function () {
     fetchGiphs();
-  }, 600);
+  }, 500);
 });
 
 function fetchGiphs(e) {
-  const searchTerm = document.querySelector(".search").value;
-  console.log(searchTerm);
+  searchTerm = document.querySelector(".search").value;
   if (searchTerm.length < 1) {
-    console.log("ending function");
     result.classList.add("ramama");
     banana.classList.add("ramama");
     intro.classList.remove("ramama");
+    box.classList.add("remove");
     return;
   }
-  console.log("hey I should not be running, and I like pie");
   fetch(
-    `http://api.giphy.com/v1/gifs/search?&q=${searchTerm}&limit=10&api_key=lceU9yKlVaHVuU0UMn3398cd9EhBj6Jh`
+    // URi
+    `${apiUrl}search?q=${searchTerm}&limit=15&offset=0&api_key=lceU9yKlVaHVuU0UMn3398cd9EhBj6Jh`
   )
     .then((response) => response.json())
     .then((res) => res.data)
     .then((data) => {
-      let dataArray = data;
-      console.log(dataArray.length);
-      if (dataArray.length === 0 && loading === false) {
-        console.log("yay");
+      data = data;
+      console.log(data);
+      if (data.length === 0 && loading === false) {
         banana.classList.remove("ramama");
         result.classList.add("ramama");
         intro.classList.add("ramama");
+        box.classList.add("remove");
       } else {
-        console.log("poo");
-        console.log(loading);
         result.classList.remove("ramama");
         banana.classList.add("ramama");
         intro.classList.add("ramama");
-        showGiphs(dataArray);
+        box.classList.remove("remove");
+        showGiphs(data);
       }
     });
 
-  function showGiphs(dataArray) {
+  function showGiphs(data) {
     let HTMLPayload = "";
 
-    for (let i = 0; i < dataArray.length; i++) {
+    for (let i = 0; i < data.length; i++) {
       const img = `
         <div class="grid-item">
-          <img class="fade-in" src="${dataArray[i].images.original.url}" />
+          <img class="fade-in" src="${data[i].images.original.url}" />
         </div>`;
       HTMLPayload += img;
     }
 
     result.innerHTML = HTMLPayload;
+    box.classList.remove("remove");
+    box.classList.remove("remove");
+    box.classList.add("loading");
   }
 }
+
+window.addEventListener("scroll", function timer() {
+  clearTimeout(timeout);
+  timeout = setTimeout(function () {
+    poo();
+  }, 2000);
+});
+
+function poo(date) {
+  if (
+    window.scrollY + window.innerHeight + 100 >=
+    document.documentElement.scrollHeight
+  ) {
+    fetch(
+      // URi
+      `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=lceU9yKlVaHVuU0UMn3398cd9EhBj6Jh&limit=${limit}&offset=${paginate}&rating=g`
+    )
+      .then((res) => res.json())
+      .then((resp) => resp.data)
+      .then((data) => {
+        // data = data;
+        paginate = paginate + 5;
+        showMoreGiphs(data);
+      });
+  }
+}
+let HTMLPayload;
+function showMoreGiphs(data) {
+  HTMLPayload = "";
+
+  for (let i = 0; i < data.length; i++) {
+    const img = `
+      <div class="grid-item">
+        <img class="fade-in" src="${data[i].images.original.url}" />
+      </div>`;
+    HTMLPayload += img;
+  }
+
+  result.innerHTML += HTMLPayload;
+}
+console.log(HTMLPayload);
+let modal = document.querySelector("modal");
+result.addEventListener("click", openModal);
+
+function openModal(e) {
+  console.log(e.target.classList);
+  if (e.target && e.target.classList == "fade-in") {
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active");
+    var modal = document.querySelector(".modal");
+    modal.classList.toggle("active");
+    let gif = e.srcElement.currentSrc;
+    const pidg = `<img class="modal-content" src="${gif}" />`;
+    modal.innerHTML += pidg;
+    console.log(e);
+    // console.log((modal.innerHTML += gif));
+  }
+}
+
+document.addEventListener("click", (e) => {
+  if (e.srcElement.children[0]) {
+    var blur = document.getElementById("blur");
+    blur.classList.remove("active");
+    var modal = document.querySelector(".modal");
+    modal.classList.remove("active");
+  }
+});
